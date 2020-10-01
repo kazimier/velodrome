57a31a719969d1a5cd6475959a60b6defa0b90ba
venya krutikov <venya@thekazimier.co.uk>
2020-10-01 12:43:58 +0100

    Add files via upload

--- Hall_Effects_V3.ino
+++ Hall_Effects_V3.ino
@@ -4,6 +4,9 @@
 #include <EthernetUdp.h>
 #include <SPI.h>    
 #include <OSCMessage.h>
+#include "RunningAverage.h"
+
+RunningAverage myRA(200);
 
 EthernetUDP Udp;
 
@@ -19,8 +22,10 @@ const int PASPin = 2;    // input from PAS
 
 
 //Software constants
-const unsigned long activityTimeoutMS = 1000; // Allowed PAS signal inactivity time before turning off
+const unsigned long activityTimeoutMS = 10000; // Allowed PAS signal inactivity time before turning off
 const int startPulses = 2; // Number of PAS pulses needed before turning on
+unsigned long timer = 0;
+int vel = 0;
 
 // Variables
 volatile int inputEdges = 0; // counter for the number of pulses since last reset
@@ -34,7 +39,7 @@ void setup() {
   pinMode(PASPin, INPUT); // initialize the PAS pin as a input
   attachInterrupt(digitalPinToInterrupt(PASPin), pulse, RISING); //Each rising edge on PAS pin causes an interrupt
     // initialize serial communication at 9600 bits per second:
-  
+  myRA.clear(); // explicitly start clean
   
 }
 
@@ -56,8 +61,28 @@ void loop() {
 
     }
   }
+  
 
+    //Serial.print("Distance: ");
+   // Serial.println(inputEdges*0.1667);
+     OSCMessage msg("/Bike1/Rotations/");
+     msg.add((float)inputEdges*0.0001);
+  
+//  msg.add((int32_t)inputEdges);
+  
+  Udp.beginPacket(outIp, outPort);
+    msg.send(Udp); // send the bytes to the SLIP stream
+  Udp.endPacket(); // mark the end of the OSC Packet
+  msg.empty(); // free space occupied by message
 
+  delay(5);
+
+  timer = millis()-lastEdgeTime;
+ 
+  vel = (166/(timer)) * 2.2;  // convert time between pulses to mph
+  myRA.addValue(vel);
+   Serial.println(myRA.getAverage(), 3);
+   //Serial.println(vel);
 }
 
 
@@ -79,21 +104,8 @@ void turnOn() {
 //Interrupt subroutine, refresh last impulse timestamp and increment pulse counter (until 10000 is reached)
 void pulse() {
   lastEdgeTime=millis();
-  if (inputEdges<100000) {
+  if (inputEdges<10000) {
     inputEdges++;
-//    Serial.print("Rotations: ");
-//    Serial.print(inputEdges);
-     OSCMessage msg("/Bike1/Rotations/");
-     msg.add((float)inputEdges*0.001);
-  
-//  msg.add((int32_t)inputEdges);
-  
-  Udp.beginPacket(outIp, outPort);
-    msg.send(Udp); // send the bytes to the SLIP stream
-  Udp.endPacket(); // mark the end of the OSC Packet
-  msg.empty(); // free space occupied by message
 
-  delay(5);
-    
   }  
 }
